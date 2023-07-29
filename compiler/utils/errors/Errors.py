5827b2c618f7536dc4f135b41a027515f5156c01
waltech1 <walTech@protonmail.com>
2023-07-29 16:30:45 -0600

    fix errors

--- compiler/utils/errors/Errors.py
+++ compiler/utils/errors/Errors.py
@@ -1,20 +1,41 @@
-from antlr4 import *
+class ErrorListener():
+    def __init__(self) -> None:
+        self.true = False
 
-class ErrorListener(object):
-  def __init__(self) -> None:
-    self.true = False
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        print(f"Error in: {line} : {column} > {msg}")
+        self.true = True
 
-  def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
-    print(f"Bad token at line: {line} : {column}")
-    print(f"Hint: {msg}")
-    self.true = True
-    # exit(1)
+    def reportAmbiguity(
+        self,
+        recognizer,
+        dfa,
+        startIndex,
+        stopIndex,
+        exact,
+        ambigAlts,
+        configs
+    ):
+        pass
 
-  def reportAmbiguity(self, recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs):
-    pass
+    def reportAttemptingFullContext(
+        self,
+        recognizer,
+        dfa,
+        startIndex,
+        stopIndex,
+        conflictingAlts,
+        configs
+    ):
+        pass
 
-  def reportAttemptingFullContext(self, recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs):
-    pass
-
-  def reportContextSensitivity(self, recognizer, dfa, startIndex, stopIndex, prediction, configs):
-    pass
+    def reportContextSensitivity(
+        self,
+        recognizer,
+        dfa,
+        startIndex,
+        stopIndex,
+        prediction,
+        configs
+    ):
+        pass
